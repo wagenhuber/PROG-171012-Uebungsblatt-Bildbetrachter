b1f6a4ca6498f74a6121421670383c89ef4da101
wagenhuber <wagenhuber@gmx.net>
2017-10-12 09:59:50 +0200

    initial commit

---
+++ src/com/sabel/bildbetrachter.java
@@ -0,0 +1,17 @@
+package com.sabel;
+
+import javax.swing.*;
+
+public class bildbetrachter {
+
+JRadioButton
+
+
+
+
+
+    public static void main(String[] args) {
+        new bildbetrachter();
+    }
+
+}
